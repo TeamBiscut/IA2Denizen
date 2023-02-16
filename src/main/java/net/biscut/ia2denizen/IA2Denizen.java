6c2f633b544e6b56bc2d764fed1cfb4be21ad409
imHinotori <hello@hinotori.moe>
2023-02-16 18:05:25 -0300

    Skeleton

---
+++ src/main/java/net/biscut/ia2denizen/IA2Denizen.java
@@ -0,0 +1,17 @@
+package net.biscut.ia2denizen;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class IA2Denizen extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
